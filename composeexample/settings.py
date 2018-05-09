3dc724fa973d7eb0d8bebea5ce403b7d4b9d0781
Michael James Kiernan <mikey@kiernan.tech>
2018-05-09 19:00:50 +0800

    Resolve: 'WARNING: No password has been set for the database.'.

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -1,9 +1,11 @@
 SECRET_KEY = 'abc'
+ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASS': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }

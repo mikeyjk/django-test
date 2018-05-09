45c30210122f433d4a1f6ffb7e28871a7a4786a9
Michael James Kiernan <mikey@kiernan.tech>
2018-05-09 19:04:26 +0800

    Resolve: 'django.db.utils.OperationalError: fe_sendauth: no password supplied'.

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -1,11 +1,13 @@
 SECRET_KEY = 'abc'
 ALLOWED_HOSTS = ['*']
+
+# https://docs.djangoproject.com/en/dev/ref/databases/#mysql-notes
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASS': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }

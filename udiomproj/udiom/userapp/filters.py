4675c5b47e5fc3cdbd193469f24a7d0f09e9abb3
Michael James Kiernan <mikey@kiernan.tech>
2018-05-11 21:50:43 +0800

    Search substr not exact match, no-one expects exact match.

--- udiomproj/udiom/userapp/filters.py
+++ udiomproj/udiom/userapp/filters.py
@@ -2,6 +2,8 @@ from .models import Person
 import django_filters
 
 class PersonFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+    email = django_filters.CharFilter(lookup_expr='icontains')
     class Meta:
         model = Person
         fields = '__all__'

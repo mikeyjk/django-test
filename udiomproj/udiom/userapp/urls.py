c2d810a0403eb36ca32af79104f069bda0431ab5
Michael James Kiernan <mikey@kiernan.tech>
2018-05-11 19:49:51 +0800

    List view now working. Validation on form is failing though.

--- udiomproj/udiom/userapp/urls.py
+++ udiomproj/udiom/userapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('people', views.Person.as_view(), name='person'),
+    path('people', views.PersonListView.as_view(), name='people'),
     path('person/create/', views.PersonCreate.as_view(), name='person_create'),
     path('person/<int:pk>/update/', views.PersonUpdate.as_view(), name='person_update'),
     path('person/<int:pk>/delete/', views.PersonDelete.as_view(), name='person_delete'),

e1e65b44e9b1f4d3f016c1bd18162bbe62893d88
Michael James Kiernan <mikey@kiernan.tech>
2018-05-11 19:58:58 +0800

    Make people list view the index of the project. Also slight clean up of list view template.

--- udiomproj/udiom/userapp/urls.py
+++ udiomproj/udiom/userapp/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('people', views.PersonListView.as_view(), name='people'),
+    path('', views.PersonListView.as_view(), name='index'),
     path('person/create/', views.PersonCreate.as_view(), name='person_create'),
     path('person/<int:pk>/update/', views.PersonUpdate.as_view(), name='person_update'),
     path('person/<int:pk>/delete/', views.PersonDelete.as_view(), name='person_delete'),
